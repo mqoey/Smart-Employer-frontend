c387a0b107ea2b00be9a4e3951a2f417f765adf1
Mqondisi <57135780+Mqoey@users.noreply.github.com>
2022-08-09 13:26:27 +0200

    add services to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DesignationService } from './services/designation/designation.service';
+import { DepartmentService } from './services/department/department.service';
+import { AttendanceService } from './services/attendance/attendance.service';
+import { EmployeeService } from './services/employee/employee.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DepartmentService,
+    DesignationService,
+    AttendanceService,
+    EmployeeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
